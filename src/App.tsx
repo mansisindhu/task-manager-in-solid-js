6e62731251d053128191e3e7775622011bbc4c8a
mansisindhu <mansisindhu2909@gmail.com>
2022-07-03 12:03:25 +0530

    Add input box and button for adding todo

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,25 @@ import type { Component } from "solid-js";
 
 const App: Component = () => {
   return (
-    <div class="flex justify-center">
-      <h1 class="text-[32px] font-extrabold">Task Manager App in Solid JS</h1>
+    <div class="flex flex-col items-center">
+      <h1 class="text-3xl my-8 font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
+        Task Manager App Ft. Solid JS
+      </h1>
+
+      <div class="flex items-center gap-2">
+        <input
+          class="w-full p-3 mt-1 text-sm border-2 border-indigo-600"
+          id="add-task"
+          type="text"
+        />
+        <button class="relative inline-block text-sm font-medium text-indigo-600 group active:text-indigo-500 focus:outline-none focus:ring">
+          <span class="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-indigo-600 group-hover:translate-y-0 group-hover:translate-x-0"></span>
+
+          <span class="relative block px-8 py-3 bg-white border border-current">
+            Add
+          </span>
+        </button>
+      </div>
     </div>
   );
 };
